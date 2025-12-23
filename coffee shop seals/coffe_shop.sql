5a101b45769c3e85c0eb6d173c846ab940b6fdd2
Md Rakibul Hasan Shaon <shaonrakibul00@gmail.com>
2025-12-24 00:40:32 +0600

    Update coffe_shop.sql

--- coffee shop seals/coffe_shop.sql
+++ coffee shop seals/coffe_shop.sql
@@ -47,26 +47,26 @@ SELECT COUNT(transaction_id) as Total_Orders
 FROM coffee_shop_sales
 WHERE MONTH (transaction_date)= 5 -- for month of (CM-May)
 
--- TOTAL ORDERS KPI - MOM DIFFERENCE AND MOM GROWTH
--- SELECT
--- MONTH(transaction_date) AS month,
--- ROUND(COUNT(transaction_id)) AS total_orders,
--- (COUNT(transaction_id) - LAG(COUNT(transaction_id), 1)
--- OVER (ORDER BY MONTH(transaction_date))) / LAG(COUNT(transaction_id), 1)
--- OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
--- FROM
--- coffee_shop_sales
--- WHERE
--- MONTH(transaction_date) IN (4, 5) -- for April and May
--- GROUP BY
--- MONTH(transaction_date)
--- ORDER BY
--- MONTH(transaction_date);
+TOTAL ORDERS KPI - MOM DIFFERENCE AND MOM GROWTH
+SELECT
+MONTH(transaction_date) AS month,
+ROUND(COUNT(transaction_id)) AS total_orders,
+(COUNT(transaction_id) - LAG(COUNT(transaction_id), 1)
+OVER (ORDER BY MONTH(transaction_date))) / LAG(COUNT(transaction_id), 1)
+OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
+FROM
+coffee_shop_sales
+WHERE
+MONTH(transaction_date) IN (4, 5) -- for April and May
+GROUP BY
+MONTH(transaction_date)
+ORDER BY
+MONTH(transaction_date);
 
--- TOTAL QUANTITY SOLD
--- SELECT SUM(transaction_qty) as Total_Quantity_Sold
--- FROM coffee_shop_sales
--- WHERE MONTH(transaction_date) = 5 -- for month of (CM-May)
+TOTAL QUANTITY SOLD
+SELECT SUM(transaction_qty) as Total_Quantity_Sold
+FROM coffee_shop_sales
+WHERE MONTH(transaction_date) = 5 -- for month of (CM-May)
 
 -- TOTAL QUANTITY SOLD KPI - MOM DIFFERENCE AND MOM GROWTH
 -- SELECT
