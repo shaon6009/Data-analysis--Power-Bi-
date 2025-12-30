10765e4c971aa59b378d32187296b7f83d7c4d6f
Md Rakibul Hasan Shaon <shaonrakibul00@gmail.com>
2025-12-31 03:56:49 +0600

    Update coffe_shop.sql

--- coffee shop seals/coffe_shop.sql
+++ coffee shop seals/coffe_shop.sql
@@ -132,44 +132,44 @@ DAY(transaction_date);
 
 -- COMPARING DAILY SALES WITH AVERAGE SALES – IF GREATER THAN “ABOVE AVERAGE” and LESSER THAN “BELOW AVERAGE”
 SELECT
--- day_of_month,
--- CASE
--- WHEN total_sales > avg_sales THEN 'Above Average'
--- WHEN total_sales < avg_sales THEN 'Below Average'
--- ELSE 'Average'
--- END AS sales_status,
--- total_sales
--- FROM (
--- SELECT
--- DAY(transaction_date) AS day_of_month,
--- SUM(unit_price * transaction_qty) AS total_sales,
--- AVG(SUM(unit_price * transaction_qty)) OVER () AS avg_sales
--- FROM
--- coffee_shop_sales
--- WHERE
--- MONTH(transaction_date) = 5 -- Filter for May
--- GROUP BY
--- DAY(transaction_date)
--- ) AS sales_data
--- ORDER BY
--- day_of_month;
+day_of_month,
+CASE
+WHEN total_sales > avg_sales THEN 'Above Average'
+WHEN total_sales < avg_sales THEN 'Below Average'
+ELSE 'Average'
+END AS sales_status,
+total_sales
+FROM (
+SELECT
+DAY(transaction_date) AS day_of_month,
+SUM(unit_price * transaction_qty) AS total_sales,
+AVG(SUM(unit_price * transaction_qty)) OVER () AS avg_sales
+FROM
+coffee_shop_sales
+WHERE
+MONTH(transaction_date) = 5 -- Filter for May
+GROUP BY
+DAY(transaction_date)
+) AS sales_data
+ORDER BY
+day_of_month;
 
--- SALES BY WEEKDAY / WEEKEND:
--- SELECT
--- CASE
--- WHEN DAYOFWEEK(transaction_date) IN (1, 7) THEN 'Weekends'
--- ELSE 'Weekdays'
--- END AS day_type,
--- ROUND(SUM(unit_price * transaction_qty),2) AS total_sales
--- FROM
--- coffee_shop_sales
--- WHERE
--- MONTH(transaction_date) = 5 -- Filter for May
--- GROUP BY
--- CASE
--- WHEN DAYOFWEEK(transaction_date) IN (1, 7) THEN 'Weekends'
--- ELSE 'Weekdays'
--- END;
+SALES BY WEEKDAY / WEEKEND:
+SELECT
+CASE
+WHEN DAYOFWEEK(transaction_date) IN (1, 7) THEN 'Weekends'
+ELSE 'Weekdays'
+END AS day_type,
+ROUND(SUM(unit_price * transaction_qty),2) AS total_sales
+FROM
+coffee_shop_sales
+WHERE
+MONTH(transaction_date) = 5 -- Filter for May
+GROUP BY
+CASE
+WHEN DAYOFWEEK(transaction_date) IN (1, 7) THEN 'Weekends'
+ELSE 'Weekdays'
+END;
 
 -- SALES BY STORE LOCATION
 -- SELECT
